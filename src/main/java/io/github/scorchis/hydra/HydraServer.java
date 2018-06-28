aed567b111355a2d4233f9745b2f6b5b306e9ace
virustotalop <virustotalop@gmail.com>
2018-06-28 15:13:25 -0700

    Started on HydraServer

--- src/main/java/io/github/scorchis/hydra/HydraServer.java
+++ src/main/java/io/github/scorchis/hydra/HydraServer.java
@@ -1,7 +1,32 @@
 package io.github.scorchis.hydra;
 
+import io.github.scorchis.hydra.plugin.PluginManager;
+import io.github.scorchis.hydra.plugin.pf4j.Pf4jHydraPluginManager;
+
 public class HydraServer implements Server {
 
+	private Pf4jHydraPluginManager pluginManager;
+	public HydraServer()
+	{
+		this.pluginManager = new Pf4jHydraPluginManager();
+	}
 	
+	@Override
+	public PluginManager getPluginManager() 
+	{
+		return this.pluginManager;
+	}
 
+	@Override
+	public void start() 
+	{
+		this.pluginManager.loadPlugins();
+		this.pluginManager.startPlugins();
+	}
+
+	@Override
+	public void stop() 
+	{
+		this.pluginManager.stopPlugins();
+	}
 }
