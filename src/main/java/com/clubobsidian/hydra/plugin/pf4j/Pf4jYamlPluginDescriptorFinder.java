949ed66392d787d6ab9b9fcaf0e2ad6c477fb083
virustotalop <virustotalop@gmail.com>
2018-09-04 04:50:30 -0700

    Drop support for cfg4j
    Drop support for cfg4j added wrappy

--- src/main/java/com/clubobsidian/hydra/plugin/pf4j/Pf4jYamlPluginDescriptorFinder.java
+++ src/main/java/com/clubobsidian/hydra/plugin/pf4j/Pf4jYamlPluginDescriptorFinder.java
@@ -1,7 +1,12 @@
 package com.clubobsidian.hydra.plugin.pf4j;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
 
 import org.pf4j.DefaultPluginDescriptor;
 import org.pf4j.PluginDependency;
@@ -10,8 +15,8 @@ import org.pf4j.PluginDescriptorFinder;
 import org.pf4j.PluginException;
 import org.pf4j.util.FileUtils;
 
-import com.clubobsidian.hydra.plugin.config.PluginConfiguration;
-import com.clubobsidian.hydra.plugin.config.PluginConfigurationProvider;
+import com.clubobsidian.wrappy.Configuration;
+import com.clubobsidian.wrappy.ConfigurationType;
 
 public class Pf4jYamlPluginDescriptorFinder implements PluginDescriptorFinder {
 
@@ -24,22 +29,52 @@ public class Pf4jYamlPluginDescriptorFinder implements PluginDescriptorFinder {
 	@Override
 	public PluginDescriptor find(Path pluginPath) throws PluginException 
 	{
-		
-		PluginConfigurationProvider configurationProvider = new PluginConfigurationProvider(pluginPath.toString());
-		PluginConfiguration pluginConfig = new PluginConfiguration(configurationProvider);
-		String name = pluginConfig.getName();
-		String description = "";
-		String main = pluginConfig.getMain();
-		String version = pluginConfig.getVersion();
-		String requires = "*";
-		String provider = "";
-		String license = "";
-		DefaultPluginDescriptor pluginDescriptor = new DefaultPluginDescriptor(name, description, main, version, requires, provider, license);
-		String[] dependencies = pluginConfig.getDependencies();
-		for(String depend : dependencies)
+
+		JarFile jar = null;
+		try 
 		{
-			pluginDescriptor.addDependency(new PluginDependency(depend));
+			jar = new JarFile(pluginPath.toFile());
+			ZipEntry entry = jar.getEntry("plugin.yml");
+			if(entry != null)
+			{
+				InputStream pluginStream = jar.getInputStream(entry);
+				Configuration pluginConfig = Configuration.load(pluginStream, ConfigurationType.YAML);
+
+				String name = pluginConfig.getString("name");
+				String description = "";
+				String main = pluginConfig.getString("main");
+				String version = pluginConfig.getString("version");
+				String requires = "*";
+				String provider = "";
+				String license = "";
+				DefaultPluginDescriptor pluginDescriptor = new DefaultPluginDescriptor(name, description, main, version, requires, provider, license);
+				List<String> dependenciesList = pluginConfig.getStringList("dependencies");
+				String[] dependencies = dependenciesList.toArray(new String[dependenciesList.size()]);
+				for(String depend : dependencies)
+				{
+					pluginDescriptor.addDependency(new PluginDependency(depend));
+				}
+				pluginStream.close();
+
+				return pluginDescriptor;
+			}
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
 		}
-		return pluginDescriptor;
+		finally
+		{
+			try 
+			{
+				jar.close();
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}
+		}
+		return null;
+
 	}
 }
